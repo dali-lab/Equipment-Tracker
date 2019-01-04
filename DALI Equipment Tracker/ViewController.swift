9a9692446c2b033a50a6ea75866dab282cd02055
John Kotz <john.lyme@mac.com>
2019-01-03 21:59:56 -0500

    Initial commit

---
+++ DALI Equipment Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DALI Equipment Tracker
+//
+//  Created by John Kotz on 1/3/19.
+//  Copyright © 2019 DALI Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
